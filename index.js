b64461da8e023f09961ff2cadfb44ef067bf2f0a
Jennifer <jenninyambu@gmail.com>
2025-06-06 20:37:25 +0300

    all changes in index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+// Write your solution here! 
+
+
+
+const cats = ["Milo", "Otis", "Garfield", "Felix"];
+
+function addCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+console.log(addCat("Felix")); // ["Milo", "Otis", "Garfield", "Felix"]
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+ function appendCat(name) {
+  return [...cats, name];
+}
+function prependCat(name) {
+  return [name, ...cats];
+}
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
